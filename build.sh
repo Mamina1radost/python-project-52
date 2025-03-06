d76813247ee4c8b4d3f23f1dcb7cc320c3484ceb
Slavyan <GlockOglock@yandex.ru>
2025-03-06 12:36:32 +0300

    rrrrnnnndddd

--- build.sh
+++ build.sh
@@ -5,4 +5,5 @@ source $HOME/.local/bin/env
 
 # здесь добавьте все необходимые команды для установки вашего проекта
 # команду установки зависимостей, сборки статики, применения миграций и другие
+export PATH=$PATH:$HOME/.local/bin
 pip3 install gunicorn && make install && make collectstatic && make migrate
