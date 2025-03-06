a760ff5424002fdd0468325bb89d5c1ffaa5d041
Slavyan <GlockOglock@yandex.ru>
2025-03-06 13:26:42 +0300

    so what

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -120,7 +120,7 @@ AUTH_USER_MODEL = 'users.User'
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
